185a5ae1acc9c96aea3cdc4499a377dc4c859464
Yutaka Kobayashi <48763691+kobayashiyutaka@users.noreply.github.com>
2022-08-05 09:51:36 +0900

    Update cljoin, main

--- cljoin.py
+++ cljoin.py
@@ -11,8 +11,17 @@ def cljoin():
         return s
     return inner
 '''
-注: クロージャの名前空間で定義されている
+注: このやり方では、クロージャの名前空間で定義されている
 文字列sが空文字（初期状態)のときと、それ
 以外の場合で文字列連結の処理を分けるのが
 ポイントです。
 '''
+# %%
+# 別解: リストを使ったやり方
+def cljoin2():
+    s = []
+    def inner(w):
+        nonlocal s
+        s.append(w)
+        return ' '.join(s)
+    return inner
--- main.py
+++ main.py
@@ -5,10 +5,21 @@ for i in range(20):
     print(next(cy))
 
 # %%
-from cljoin import *
+from cljoin import cljoin
 cl = cljoin()
 print(cl('My'))
 print(cl('name'))
 print(cl('is'))
 print(cl('Taro.'))
 
+
+# %%
+from cljoin import cljoin2
+cl2 = cljoin2()
+print(cl2('My'))
+print(cl2('name'))
+print(cl2('is'))
+print(cl2('Taro.'))
+
+
+# %%
